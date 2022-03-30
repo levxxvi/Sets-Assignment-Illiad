9f84497cde036005f3c5b3f8f74a4e98781b3289
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-03-30 14:11:45 -0700

    added the iliad

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * use linked hash sets? since it won't add existing elements,
+ * just check size after for unique words
+ */
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(new File("Iliad.txt"));
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Iliad.txt");
+        Scanner scanner = new Scanner(file);
         while (scanner.hasNext()) {
             System.out.println(scanner.nextLine());
         }
