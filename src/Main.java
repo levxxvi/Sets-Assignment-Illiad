fcc4cccc12b585b1415aa1bf7b37a233e5439c7c
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-03-30 22:09:15 -0700

    done :D

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,13 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.TreeSet;
-
-/**
- * use linked hash sets? since it won't add existing elements,
- * just check size after for unique words
- */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("Iliad.txt");
         Scanner scanner = new Scanner(file);
-        /*while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
-        }
-        while (scanner.hasNext()) {
-            System.out.println(scanner.next());
-        }*/
-
-        //turn file to string? string to tree set?
-        /*Path iliadPath = file.toPath();
-        String content = Files.readString(iliadPath, StandardCharsets.US_ASCII);*/
-
-        /*TreeSet<String> uniqueWords = new TreeSet<>();
-        while(scanner.hasNext()){
-            uniqueWords.add(scanner.next());
-        }
-
-        //edit this to replace all punct in iliad
-        String iliadText = scanner.nextLine();
-        String noPunct = iliadText.replaceAll("\\p{Punct}", "");
-        System.out.println(noPunct);
-
-        Iterator<String> wordIterator = uniqueWords.iterator();
-        while (wordIterator.hasNext()){
-            System.out.println(wordIterator.next());
-        }
-        System.out.println(uniqueWords.size());*/
         TreeSet<String> uniqueWords = new TreeSet<>();
         while(scanner.hasNext()){
             String iliadText = scanner.next();
